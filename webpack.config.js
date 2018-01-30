37c1022e14dd90e70164213c843aaf6414e3e838
Rudi Olckers <rudi.olckers@gmail.com>
2018-01-30 23:51:44 +1300

    Fix public path to use dist folder when serving images.

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,8 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: '[name].[ext]?[hash]',
+          publicPath: './dist/'
         }
       }
     ]
